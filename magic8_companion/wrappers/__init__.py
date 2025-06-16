6d52275aec5c1a4a889a0cac0f8ba2cafdd30649
birddograbbit <birddograbbit@gmail.com>
2025-06-16 20:39:38 +0200

    Add EnhancedGEXWrapper to wrappers module

--- magic8_companion/wrappers/__init__.py
+++ magic8_companion/wrappers/__init__.py
@@ -5,5 +5,6 @@ Ship-fast approach: Simple interfaces to mature systems.
 from .greeks_wrapper import GreeksWrapper
 from .gex_wrapper import GammaExposureWrapper
 from .volume_wrapper import VolumeOIWrapper
+from .enhanced_gex_wrapper import EnhancedGEXWrapper
 
-__all__ = ['GreeksWrapper', 'GammaExposureWrapper', 'VolumeOIWrapper']
+__all__ = ['GreeksWrapper', 'GammaExposureWrapper', 'VolumeOIWrapper', 'EnhancedGEXWrapper']
