326997d56be24672b91d07d74fd5aad73269ff48
birddograbbit <birddograbbit@gmail.com>
2025-06-17 21:52:28 +0200

    Create gamma module init file

--- magic8_companion/analysis/gamma/__init__.py
+++ magic8_companion/analysis/gamma/__init__.py
@@ -1,7 +1,17 @@
 """
-Gamma Analysis Module
+Gamma Analysis Module for Magic8-Companion.
+Native implementation of Enhanced Gamma Exposure analysis.
 """
+from .calculator import GammaExposureCalculator
+from .levels import GammaLevels
+from .regime import MarketRegimeAnalyzer
 
-from .gamma_exposure import GammaExposureAnalyzer
+__all__ = [
+    'GammaExposureCalculator',
+    'GammaLevels',
+    'MarketRegimeAnalyzer'
+]
 
-__all__ = ['GammaExposureAnalyzer']
+# Version info
+__version__ = '1.0.0'
+__author__ = 'Magic8-Companion Team'
